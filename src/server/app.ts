ff8d2819504e81bff90fa6bc83c4b892db839eae
GnatNamedAsh <33440472+GnatNamedAsh@users.noreply.github.com>
2025-04-18 10:03:17 -0500

    add in some documentation for future implementations

--- src/server/app.ts
+++ src/server/app.ts
@@ -4,6 +4,14 @@ import { get } from './routes/weather/handlers'
 
 export const app = express()
 
+// If we were to expand out the API to include hourly, daily, etc.
+// We'd want to dynamically set these routes using some kind of
+// recursive directory algorithm. Everything in /routes will
+// directly relate to the endpoint. e.g. endpoint handlers for
+// `/weather/current` will be in `/routes/weather/current/handlers`
+// and `/weather/hourly` will be in `/routes/weather/hourly/handlers`
+// we can still have `/weather` under `/routes/weather/handlers` and
+// adjust its implementation there.
 app.get('/weather', get)
 
 // Only start the server if this file is run directly
